8c3e6623c8373f928a78e224ca35cd0f3067f732
vctrwesley <vctrwesley@gmail.com>
2024-03-21 13:24:42 -0300

    Modificação na navbar e na resolução da responsividade

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -8,12 +8,16 @@ $(document).ready(function () {
             'projects': $('#projects').offset().top,
             'contato': $('#contato').offset().top
         };
-        
+
         $.each(sections, function (key, value) {
             if (scrollPosition >= value - 100) {
                 $('nav .navbar-nav .nav-item').removeClass('active');
                 $('nav .navbar-nav .nav-item:eq(' + $("section").index($('#' + key)) + ')').addClass('active');
             }
         });
+
+        // Remover '#' da URL OBS: temporario
+        //var currentPage = window.location.href.split('#')[0];
+        //history.replaceState({}, document.title, currentPage);
     });
 });
